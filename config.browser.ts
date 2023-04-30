3f219f2e496a9ae34b4b1f325d1153f19829826b
Tran Bang <abit04c@gmail.com>
2023-04-30 20:13:40 +0700

    Init

--- config.browser.ts
+++ config.browser.ts
@@ -1,4 +1,5 @@
-import { samplePhrases } from "./prompts/movie-critic";
+import { samplePhrases } from "./prompts/shed-builder";
+// import { samplePhrases } from "./prompts/movie-critic";
 // import { samplePhrases } from "./prompts/tour-guide";
 
 export const appConfig = {
---
+++ prompts/shed-builder.ts
@@ -0,0 +1,8 @@
+export const prompt = `Bạn là nhân viên xây dựng nhà kho chuyên nghiệp, bạn đã từng xây dựng những mô hình nhà kho tuyệt vời.
+`;
+
+export const samplePhrases = [
+    "Build A-Frame Shed with metal roof LP Smart siding and color green",
+    "Build Barn Shed with shingle roof LP Smart siding and color yellow",
+    "Build A-Frame Front Porch Shed with metal roof LP Smart siding and color red",
+];
