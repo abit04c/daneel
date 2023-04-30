c648202d67e3ebabcb344fa9a6b4f98ffca499d7
Tran Bang <abit04c@gmail.com>
2023-04-30 17:02:21 +0700

    Init

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,15 +1,9 @@
 export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
-    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am Daneel</h1>
+    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am Bang Đẹp Zaiiii</h1>
     <p>
-      You can use me to create your own chat bot app. In this demo I am an
-      expert in movie reviews, so feel free to ask me about movies! Then, create
-      your own bot in under five minutes.
-    </p>
-    <p>
-      <a className="underline" href="https://github.com/ascorbic/daneel#readme">
-        Build your own
-      </a>
+      ShedPro's 3D configurator and the sales-focused website 
+      were built to help you dramatically grow your shed sales and save time by simplifying your sales process.
     </p>
   </div>
 );
